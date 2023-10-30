7c403582850b715a9460d0b1180e245b03bc2608
parth <parthmandhani78@icloud.com>
2023-10-31 01:13:11 +0530

    DOne only slcde

--- ArrayMethods.js
+++ ArrayMethods.js
@@ -1,1 +1,6 @@
 //push pop slic splice shift methods in Array
+var animals = ["Dogs","Cats","humans","elephants"];
+console.log(animals);
+
+console.log(animals.slice(2,3)); // from 2 to 3 excluding 3
+
