6b1f382f4d4df85ebc9543c7ec8b17bdab34071e
parth <parthmandhani78@icloud.com>
2023-11-02 02:57:08 +0530

    basics of java script -2

---
+++ JSbasic2.js
@@ -0,0 +1,6 @@
+var a = "you" ;
+var b = "genius";
+console.log(a + "are very " + b);
+console.log(`${a} are very ${b}`);  // sencond way to write string in log using backtick (`)
+
+console.log(typeof(b)); //prints the data type of b 
